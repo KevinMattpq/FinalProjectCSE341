1ff03cfa947bb294c27021f1a32e2b0b75927cb7
KevinMattpq <kevinmpena05@gmail.com>
2023-11-22 15:43:17 -0800

    Revert "Week11 Fist Commit"
    This reverts commit 8f2269e6e461838e65d5f175e51d7f78c43a16e1.

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-const express = require('express');
-const app = express();
-var bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-const cors = require('cors');
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res) {
   res.send('SoccerTransfer Pro - Roguin Espinal And Kevin Pena')
 })
-
-// Parece que no esta funcionando la CONEXION CON MONGO DB
-
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
 
 app.listen(port)
