650c0ed537ede3b234e048acbfd84a4c5f6ad4a5
KevinMattpq <kevinmpena05@gmail.com>
2023-11-22 15:51:18 -0800

    W11 First Commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res) {
   res.send('SoccerTransfer Pro - Roguin Espinal And Kevin Pena')
 })
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
 
 app.listen(port)
