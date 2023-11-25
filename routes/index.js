87844a5eba89db9f1621a743ef1d6ff6ed26c9ef
KevinMattpq <kevinmpena05@gmail.com>
2023-11-24 23:31:51 -0800

    Last Updates W11

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const agents = require('./agents');
 
 // router.use('/', require('./swagger'));
-router.use('/agents', require('./agents'));
-router.use('/appointments', require('./appointments'));
-router.use('/clubs', require('./clubs'));
-router.use('/players', require('./players'));
+router.use('/agents', agents);
+// router.use('/appointments', require('./appointments'));
+// router.use('/clubs', require('./clubs'));
+// router.use('/players', require('./players'));
 
 module.exports = router;
