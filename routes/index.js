d5faee666d2dd407b3ba36f5f20768cbf067ccf0
KevinMattpq <kevinmpena05@gmail.com>
2023-12-09 06:21:05 -0800

    Adding Auth0

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 // Adding Auth0
-// const { auth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
 
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: 'a long, randomly-generated string stored in env',
-//   baseURL: 'http://localhost:4000',
-//   clientID: '0uu8nxzWFt5WHUIG9romvo5fjuvKYaoV',
-//   issuerBaseURL: 'https://dev-hilr1ws8ejjafmrv.us.auth0.com'
-// };
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'https://cse-341kp07.onrender.com',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.SECRET_KEY
+};
 
 
 
