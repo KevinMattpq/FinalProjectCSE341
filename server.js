407a8841aea27f8225832f8df3f1fb8dddd8b601
KevinMattpq <kevinmpena05@gmail.com>
2023-12-09 07:59:52 -0800

    Push

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+
 app
   .use(cors())
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
