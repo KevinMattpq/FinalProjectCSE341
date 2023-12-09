967a9fcb46d9fd5ba219290aabbab0b177749480
KevinMattpq <kevinmpena05@gmail.com>
2023-12-09 06:44:50 -0800

    Adding auth0 portion

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,19 @@ const { auth } = require('express-openid-connect');
 const config = {
   authRequired: false,
   auth0Logout: true,
-  baseURL: 'https://cse-341kp07.onrender.com',
+  baseURL: 'https://soccertransferpro.onrender.com',
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
   secret: process.env.SECRET_KEY
 };
 
-
+ // auth router attaches /login, /logout, and /callback routes to the baseURL
+ router.use(auth(config));
+  
+ // req.isAuthenticated is provided from the auth router
+ router.get('/', (req, res) => {
+   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+ });
 
 const agents = require('./agents');
 const appointments = require('./appointments');
